cddfd74cbb534abf4f42d08ff5b0b7f6be5b480a
Marios Georgiou <mgeorgiou.ece@gmail.com>
2022-03-31 09:57:55 -0400

    experiment on predictor standardization

--- prostate-data/correlation.py
+++ prostate-data/correlation.py
@@ -4,4 +4,8 @@ import pandas as pd
 if __name__ == "__main__":
     X_train = pd.read_csv('./prostate.data', delim_whitespace=True)
     X_train = X_train[X_train['train'] == 'T']
+    X_train = X_train.drop(columns='train')
     print(X_train.corr().round(5))
+
+    X_train_norm = (X_train-X_train.mean())/X_train.std()
+    print(X_train_norm.corr().round(5))
