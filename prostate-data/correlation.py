fbfcac59a699d47c24cfc4de20b9e95d67d1cbad
Marios Georgiou <mgeorgiou.ece@gmail.com>
2022-03-30 21:13:11 -0400

    notes on correlation

--- prostate-data/correlation.py
+++ prostate-data/correlation.py
@@ -4,4 +4,4 @@ import pandas as pd
 if __name__ == "__main__":
     X_train = pd.read_csv('./prostate.data', delim_whitespace=True)
     X_train = X_train[X_train['train'] == 'T']
-    print(X_train.corr())
+    print(X_train.corr().round(5))
