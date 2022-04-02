164b823cdd299cc83f5d93fce04890f19d1cced9
Marios Georgiou <mgeorgiou.ece@gmail.com>
2022-04-01 21:36:14 -0400

    experiment with f-statistic

--- prostate-data/correlation.py
+++ prostate-data/correlation.py
@@ -1,26 +1,41 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+from sklearn.metrics import mean_squared_error
 
 
 if __name__ == "__main__":
     Data = pd.read_csv('./prostate.data', delim_whitespace=True)
-    Data = Data[Data['train'] == 'T']
-    Data = Data.drop(columns='train')
+    Data_train = Data[Data['train'] == 'T']
+    Data_train = Data_train.drop(columns='train')
+    Data_test = Data[Data['train'] == 'F']
+    Data_test = Data_test.drop(columns='train')
+    print('Correlation Matrix')
     print(Data.corr().round(5))
 
-    # Data_norm = (Data-Data.mean())/Data.std()
-
     scaler = StandardScaler()
-    Data_strd = scaler.fit_transform(Data)
-    Data_strd = pd.DataFrame(Data_strd, columns=Data.columns)
+    Data_strd = scaler.fit_transform(Data_train)
+    Data_strd = pd.DataFrame(Data_strd, columns=Data_train.columns)
 
-    X_train = Data.iloc[:, :-1]
-    y_train = Data.iloc[:, -1]
+    X_train = Data_strd.iloc[:, :-1]
+    y_train = Data_strd.iloc[:, -1]
 
-    # print(Data_norm - Data_strd)
+    X_train = sm.add_constant(X_train)
+    model2 = sm.OLS(y_train, X_train)
+    results = model2.fit()
+    print(results.summary())
 
-    model = LinearRegression().fit(X_train, y_train)
-    print(model.coef_)
-    print(model.intercept_)
-    # print(model.)
+    hypothesis = '(age = 0), (lcp = 0), (gleason = 0), (pgg45 = 0)'
+    print(results.f_test(hypothesis))
+
+
+    Data_strd = scaler.fit_transform(Data_test)
+    Data_strd = pd.DataFrame(Data_strd, columns=Data_test.columns)
+
+    X_test = Data_strd.iloc[:, :-1]
+    y_test = Data_strd.iloc[:, -1]
+
+    X_test = sm.add_constant(X_test)
+    y_pred = results.predict(X_test)
+    print(mean_squared_error(y_pred, y_test))
+
