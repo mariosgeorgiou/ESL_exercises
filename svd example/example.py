1d8bb447afabcbcfcb62debd71ca16593c0a6ef9
Marios Georgiou <mgeorgiou.ece@gmail.com>
2022-04-11 21:43:50 -0400

    more on svd

--- svd example/example.py
+++ svd example/example.py
@@ -3,11 +3,9 @@ from numpy.linalg import svd
 from numpy import outer
 
 image = imread('hbsZF.png')
-print(image.shape)
-
 
 u, s, vh = svd(image)
-print(u.shape, s.shape, vh.shape)
 
-im = outer(u[0],vh[0])
-imwrite('out.png',im)
+result = sum([s[i]*outer(u[:, i], vh[i]) for i in range(0, 1)])
+
+imwrite('out.png', result)
